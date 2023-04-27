bdc659b2766f6e04cebcb7b3ccbb749ffd2bb9cd
SoohoLee <hyeri0603@naver.com>
2023-04-27 23:32:04 +0900

    project init

---
+++ src/main/java/com/example/webservicewithspringbootandaws/WebServiceWithSpringBootAndAwsApplication.java
@@ -0,0 +1,13 @@
+package com.example.webservicewithspringbootandaws;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebServiceWithSpringBootAndAwsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WebServiceWithSpringBootAndAwsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/webservicewithspringbootandaws/WebServiceWithSpringBootAndAwsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.webservicewithspringbootandaws;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebServiceWithSpringBootAndAwsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
